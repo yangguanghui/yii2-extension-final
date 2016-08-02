8a5c0be2c92158c8797d924e52a5da450de3f0fa
yangguanghui <252699464@qq.com>
2016-08-02 10:41:57 +0800

    use gii as template

--- views/default/index.php
+++ views/default/index.php
@@ -1,12 +1,30 @@
-<div class="modFinal-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+<?php
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View */
+/* @var $generators \yii\gii\Generator[] */
+/* @var $content string */
+
+$generators = Yii::$app->controller->module->generators;
+$this->title = 'Welcome to Gii';
+?>
+<div class="default-index">
+    <div class="page-header">
+        <h1>Welcome to Gii <small>a magical tool that can write code for you</small></h1>
+    </div>
+
+    <p class="lead">Start the fun with the following code generators:</p>
+
+    <div class="row">
+        <?php foreach ($generators as $id => $generator): ?>
+        <div class="generator col-lg-4">
+            <h3><?= Html::encode($generator->getName()) ?></h3>
+            <p><?= $generator->getDescription() ?></p>
+            <p><?= Html::a('Start &raquo;', ['default/view', 'id' => $id], ['class' => 'btn btn-default']) ?></p>
+        </div>
+        <?php endforeach; ?>
+    </div>
+
+    <p><a class="btn btn-success" href="http://www.yiiframework.com/extensions/?tag=gii">Get More Generators</a></p>
+
 </div>
